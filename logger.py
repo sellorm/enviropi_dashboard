0ba6ba438061a46807d34ac03e1b90ab4513b539
Mark Sellors <github@5vcc.com>
2023-03-21 22:35:11 +0000

    Calibrate temperature to compensate for CPU temp

--- logger.py
+++ logger.py
@@ -4,12 +4,24 @@ timestamp = datetime.utcnow().isoformat()
 
 import sensor
 
+def temp_calibrated(sensor_temp, adjustment):
+    # The first read is discarded as it's unreliable
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        temp = f.read()
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        temp = f.read()
+        cpu_temp = int(temp) / 1000.0
+    # output_temp = sensor_temp - ((cpu_temp - sensor_temp) / adjustment)
+    output_temp = sensor_temp - (cpu_temp / 35)
+    return float(output_temp)
+
 data = sensor.read_all()
 
 message = "{},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}".format(
         timestamp,
         data["proximity"],
-        data["temperature"],
+        temp_calibrated(data["temperature"], 2.25),
+        # data["temperature"],
         data["pressure"],
         data["humidity"],
         data["light"],
--- temp_status.sh
+++ temp_status.sh
@@ -4,12 +4,9 @@ set -euo pipefail
 
 INPUT=${1:-default}
 
-API_TOKEN=${PUSHOVER_ENVIROPI}
-USER_KEY=${PUSHOVER_USER_KEY}
-
-TEMP_1MIN=$(tail -1 enviro_$(date -I).log | awk -F, '{ sum += $3 } END { if (NR > 0) printf("%.1f", (sum / NR))}')
-TEMP_15MINS=$(tail -15 enviro_$(date -I).log | awk -F, '{ sum += $3 } END { if (NR > 0) printf("%.1f", (sum / NR))}')
-TEMP_HOUR=$(tail -60 enviro_$(date -I).log | awk -F, '{ sum += $3 } END { if (NR > 0) printf("%.1f", (sum / NR))}')
+TEMP_1MIN=$(tail -1 ~/enviro_$(date -I).log | awk -F, '{ sum += $3 } END { if (NR > 0) printf("%.1f", (sum / NR))}')
+TEMP_15MINS=$(tail -15 ~/enviro_$(date -I).log | awk -F, '{ sum += $3 } END { if (NR > 0) printf("%.1f", (sum / NR))}')
+TEMP_HOUR=$(tail -60 ~/enviro_$(date -I).log | awk -F, '{ sum += $3 } END { if (NR > 0) printf("%.1f", (sum / NR))}')
 
 
 MESSAGE="Temperature: ${TEMP_1MIN}/${TEMP_15MINS}/${TEMP_HOUR}"
@@ -20,6 +17,9 @@ if [ "${INPUT}" == "default" ]; then
     echo "Use 'push' to send this data via pushover.net"
     exit 0
 fi
+
+API_TOKEN=${PUSHOVER_ENVIROPI}
+USER_KEY=${PUSHOVER_USER_KEY}
 
 curl -s \
     --form-string "token=${API_TOKEN}" \
